be3213660204eae1af57520fde63e4abf06f772a
Felipe.Macedo <felipealexandrej@gmail.com>
2024-10-16 14:56:53 -0300

    refactor: apply SOLID principles and improve project structure
    - Separated concerns by moving RabbitMQ connection logic to the 'queue' package
    - Created 'Queue' interface to abstract message queuing implementation
    - Moved CSV processing logic to the 'service' package for better modularity
    - Removed 'csv_handler.go' as it was responsible for multiple tasks, now refactored into respective modules
    - Improved maintainability and adherence to SOLID principles (Single Responsibility, Dependency Inversion)

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"log"
 
-	"github.com/felipemacedo1/transaction-producer-ms/internal/handler"
+	"github.com/felipemacedo1/transaction-producer-ms/internal/queue"
+	"github.com/felipemacedo1/transaction-producer-ms/internal/service"
 )
 
 func main() {
 	const filePath = "/app/input/input-data.csv"
-	err := handler.ProcessCSVFile(filePath)
+	rabbitMQ, err := queue.NewRabbitMQ("amqp://guest:guest@rabbitmq:5672/", "transaction_queue")
+	if err != nil {
+		log.Fatal("error connecting to RabbitMQ: ", err)
+	}
+	defer rabbitMQ.Close()
+
+	csvService := service.NewCSVService(rabbitMQ)
+	err = csvService.ProcessCSVFile(filePath)
 	if err != nil {
 		log.Fatal("error processing csv file: ", err)
 	}
